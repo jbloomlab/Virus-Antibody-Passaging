10f20846a2a77834f4d890143e5e4997764697d3
Will Hannon <wwh22@uw.edu>
2024-08-21 17:47:31 -0700

    Update to stable environment

--- run_analysis.bash
+++ run_analysis.bash
@@ -1,23 +1,19 @@
 #!/bin/bash
+#
+#SBATCH -c 32
+#SBATCH --time 1-0
+#SBATCH -J "{rule}"
 
 # stop on errors
 set -e
 
 echo "Running snakemake..."
 
-# Remove tmp/ dir and slurm files
-snakemake clean --cores 1
-
-# Make fresh tmp/ dir 
-mkdir -p tmp
-
-# Run the main analysis on slurm cluster
+# Run the main analysis on `slurm` cluster
 snakemake \
-    --use-conda \
-    --conda-prefix env/ \
-    # --cluster-config cluster.yml \
-    # --cluster "sbatch -p {cluster.partition} -c {cluster.cpus} -t {cluster.time} -J {cluster.name} -o ./tmp/slurm-%x.%j.out" \
+    --software-deployment-method conda \
+    --conda-frontend mamba \
+    --conda-prefix ./env \
     -j 999 \
-    --latency-wait 60
-
-echo "Run of snakemake complete."
+    --latency-wait 60 \
+    --rerun-incomplete
